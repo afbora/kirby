a01f7eb4afdbb2405f06df800a323ad9e7e41150
Ahmet Bora <byybora@gmail.com>
2020-07-16 16:26:53 +0300

    1

--- src/Cms/ModelWithContent.php
+++ src/Cms/ModelWithContent.php
@@ -552,7 +552,7 @@ abstract class ModelWithContent extends Model
         if ($languageCode !== $kirby->defaultLanguage()->code()) {
             foreach ($this->blueprint()->fields() as $field) {
                 if (($field['translate'] ?? true) === false) {
-                    $content[$field['name']] = null;
+                     unset($content[$field['name']]);
                 }
             }
 
